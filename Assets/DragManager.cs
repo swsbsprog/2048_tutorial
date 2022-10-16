9c0da64e2facf340c8e615fa1e1dfaa80f8c4394
hktrollgames <hktrollgames@gmail.com>
2022-10-16 14:07:20 +0900

    플러그인 추가

---
+++ Assets/DragManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) Move(Vector2Int.up);
+        if (Input.GetKeyDown(KeyCode.S)) Move(Vector2Int.down);
+        if (Input.GetKeyDown(KeyCode.A)) Move(Vector2Int.left);
+        if (Input.GetKeyDown(KeyCode.D)) Move(Vector2Int.right);
+    }
+
+    private void Move(Vector2Int direction)
+    {
+        print(direction);
+    }
+}
