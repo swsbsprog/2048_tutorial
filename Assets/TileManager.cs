81d0c8c75aa27611f8c641be68cdbc1f8a695a74
hktrollgames <hktrollgames@gmail.com>
2022-10-16 15:36:46 +0900

    합침

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -13,9 +13,16 @@ public class TileManager : MonoBehaviour
         print("방향 : "+direction);
 
         // 모든 타일을 순회 하면서 방향으로 이동하기
-        for (int y = 0; y < 4; y++)
+        //for (int y = 0; y < 4; y++)
+        int[] yArray = (direction == Vector2Int.down) ?
+            new int[]{ 3, 2, 1, 0 } : new int[] { 0, 1, 2, 3 };
+
+        int[] xArray = (direction == Vector2Int.left) ?
+            new int[] { 3, 2, 1, 0 } : new int[] { 0, 1, 2, 3 };
+
+        foreach (var y in yArray)
         {
-            for (int x = 0; x < 4; x++)
+            foreach (var x in xArray)
             {
                 // null이 아닐때마 움직이자
                 if(tiles[x,y] != null)
@@ -23,16 +30,33 @@ public class TileManager : MonoBehaviour
                     // 방향으로 움직이자.
                     var item = tiles[x, y];
                     var nextPos = item.pos + direction;
-                    if(IsInArea(nextPos))
-                        item.SetPosition(item.pos + direction);
+                    if (IsInArea(nextPos)) // 움직일 수 있는 영역 안에서만 이동 가능(0 ~ 3)
+                    {
+                        // 움직이는 곳에 타일이 있다면 
+                        var nextTile = tiles[nextPos.x, nextPos.y];
+                        if(nextTile != null ) // 있다면 
+                        {
+                            if(nextTile.number == item.number)
+                            {
+                                //  같다면 -> 합친다(이동가능)
+                                item.SetNumber(item.number * 2);
+                                item.SetPosition(nextPos);
+                                Destroy(nextTile.gameObject);
+                            }
+                            else  //  다르다면 이동 불가.
+                            {
+                                //item.SetPosition(nextPos);
+                            }
+                        }
+                        else
+                        {
+                            item.SetPosition(nextPos);
+                        }
+                    }
                 }
             }
         }
 
-        // 움직일 수 있는 영역 안에서만 이동 가능(0 ~ 3)
-        // 움직이는 곳에 타일이 있다면 
-        //  같다면 -> 합친다(이동가능)
-        //  다르다면 이동 불가.
     }
 
     private bool IsInArea(Vector2Int pos)
